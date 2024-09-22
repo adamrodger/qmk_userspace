3233ec24163ec976ef061a6ada15f40dab8863dd
Adam Rodger <adam.rodger@gmail.com>
2024-09-22 09:55:23 +0100

    fix: Use LAlt instead of RAlt

--- keyboards/crkbd/rev1/keymaps/adamrodger/keymap.c
+++ keyboards/crkbd/rev1/keymaps/adamrodger/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         DF(0),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        DF(1), KC_MINS, KC_RALT, KC_LCTL, KC_LSFT, KC_PLUS,                       KC_INS, KC_HOME,   KC_UP,  KC_END, KC_PGUP,  KC_F12,
+        DF(1), KC_MINS, KC_LALT, KC_LCTL, KC_LSFT, KC_PLUS,                       KC_INS, KC_HOME,   KC_UP,  KC_END, KC_PGUP,  KC_F12,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         DF(5), XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                       KC_DEL, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, CW_TOGG,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
